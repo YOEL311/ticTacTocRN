0f5f74d055cdfac954099622c631059996743cfb
Yoel31 <yoel301@gmail.com>
2021-02-03 14:17:03 +0200

    init

---
+++ App.js
@@ -0,0 +1,109 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, View, Button, Text} from 'react-native';
+
+// import Toast from 'react-native-simple-toast';
+
+const App: () => React$Node = () => {
+  const initTable = {
+    0: [null, null, null],
+    1: [null, null, null],
+    2: [null, null, null],
+  };
+  const simple = {
+    null: '-',
+    true: '*',
+    false: '0',
+  };
+
+  const [table, setTable] = React.useState(initTable);
+  const [queue, setQueue] = React.useState(true);
+  const [winner, setWinner] = React.useState(false);
+
+  const pressCell = (row, col) => {
+    if (winner) {
+      // Toast.show('Unable to continue playing', Toast.LONG);
+      return;
+    }
+    const newRow = [...table[row]];
+    newRow[col] = simple[queue];
+    setTable({...table, [row]: newRow});
+    setQueue((old) => !old);
+  };
+
+  React.useEffect(() => {
+    for (let i = 0; i < 3; i++) {
+      const first = table[i][0];
+      if (first === null) return;
+      if (
+        first === table[i][0] &&
+        first === table[i][1] &&
+        first === table[i][2]
+      ) {
+        setWinner(true);
+        displaySuccess();
+      }
+      const firstE = table[1][1];
+      if (firstE === null) return;
+      if (
+        (firstE === table[0][0] && firstE === table[2][2]) ||
+        (firstE === table[2][0] && firstE === table[0][2])
+      ) {
+        setWinner(true);
+      }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [queue]);
+  const displaySuccess = () => {
+    // Toast.show('success', Toast.LONG);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>{winner ? 'Unable to continue playing' : ''}</Text>
+      {Object.keys(table).map((row, indexRow) => {
+        return (
+          <View key={indexRow} style={styles.rowTable}>
+            {table[row].map((col, indexCol) => {
+              return (
+                <Button
+                  key={`${indexRow} ${indexCol}`}
+                  onPress={() => {
+                    pressCell(indexRow, indexCol);
+                  }}
+                  title={table[indexRow][indexCol] || '-'}
+                />
+              );
+            })}
+          </View>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rowTable: {
+    flexDirection: 'row',
+  },
+  cell: {
+    height: 20,
+    width: 30,
+    margin: 10,
+  },
+});
+
+export default App;
